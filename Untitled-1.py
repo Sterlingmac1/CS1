4c90a71c56ed7e0bf1e10aea399b8abd2e63a3c4
Sterlingmac1 <Smccall28@gcds.net>
2025-09-15 09:49:02 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,86 @@
+my_string = "hello"
+reversed_string = my_string[::-1]
+print(reversed_string)  # Output: olleh
+
+my_list = [1, 2, 3, 4, 5]
+reversed_list = my_list[::-1]
+print(reversed_list)    # Output: [5, 4, 3, 2, 1]
+def count_vowels(string):
+    '''
+    Takes a string and returns the number of vowels in it
+    Args:
+        none
+    Returns:
+        the number of vowels in a sentence
+    '''
+    num_vowels = 0
+    for character in string:
+        if
+    character in ['a','i','u','o','e']:
+            # num_vowels +=1
+    return num_vowels
+
+
+def split(full_name)
+    name = full_name.split("")
+    for order in name:
+        if len(name) <=3:
+            three_name(name)
+            return number
+        else:
+            more_name(name)
+        '''else'''
+
+def three_name(name):
+
+    first_name
+
+
+name_out=""
+for letter in name:
+    if ord(letter)>96 and ord(letter)<122:
+        number=ord(letter)
+        number=number-32
+        letter = chr(num)
+        else name_out = name_out + letter
+        return name_out
+    
+
+
+def first_name(name):
+    output = ''
+    for letter in name:
+        if letter=="":
+            break
+        else:
+            output = output+letter
+        return output
+
+def last_name(name)
+    output = ''
+    for letter in name [::-1]:
+        if letter == "":
+            break 
+        else:
+            output = output + letter
+        last_name = reversed_string(output)
+        return last_name
+
+name = 
+def lower(string)
+    '''
+    takes a letter and changes it from uppercase to lowercase
+    arg.
+    Change upercase letter(str): change a letter to lowercase 
+    returns:
+    the functions
+    '''
+for letter in name:
+    if ord(letter) 764 and ord(letter)<91
+num = ord(letter)
+num = num + 32
+letter = chr(num)
+name_out = name_out + letter
+else 
+    num_out = name_out + letter
+Return name_out
